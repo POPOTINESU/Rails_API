cc0002cfa5d50a291860160b0fecb20d727ba044
重国 <gzhongguo92@gmail.com>
2024-04-25 08:33:47 +0900

    バリデーション追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :emai, presence: true, uniqueness: true
+  validates :password_digest, presence:true, length: {minimum:8}
+
 end
