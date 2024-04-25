d606aaaf4cd91264674cee84ba89d62d95a296c3
重国 <gzhongguo92@gmail.com>
2024-04-26 01:13:27 +0900

    ユーザー追加ができるようになった

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  # before_action :user_params
+
+  #GET /users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { message: "User created successfully" }, status: 200
+    else
+      render json: { error: "残念"}, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest)
+  end
+end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :emai, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password_digest, presence:true, length: {minimum:8}
 
 end
